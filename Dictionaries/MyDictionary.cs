5c6fd89dc0fa9ddd2f18f6ecbd5c4ade6573a72f
SevtapE <sevtaperbek@outlook.com>
2021-09-14 02:34:18 +0300

    Add project files.

---
+++ Dictionaries/MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionaries
+{
+    class MyDictionary<TKey, TValue>
+    {
+
+        TKey[] _arrayKeys;
+        TValue[] _arrayValues;
+
+        TKey[] _tempArrayKey;
+        TValue[] _tempArrayValue;
+
+       
+
+        public MyDictionary()
+        {
+            _arrayKeys = new TKey[0];
+            _arrayValues = new TValue[0];
+          
+        }
+        public void Add(TKey key, TValue value)
+        {
+            //hold the old references 
+            _tempArrayKey = _arrayKeys;
+            _tempArrayValue = _arrayValues;
+
+            _arrayKeys = new TKey[_arrayKeys.Length + 1];
+            _arrayValues = new TValue[_arrayValues.Length + 1];
+
+
+            for (int i = 0; i < _tempArrayKey.Length; i++)
+            {
+                _arrayKeys[i] = _tempArrayKey[i];
+            }
+            for (int i = 0; i < _tempArrayValue.Length; i++)
+            {
+                _arrayValues[i] = _tempArrayValue[i];
+            }
+
+            _arrayKeys[_arrayKeys.Length - 1] = key;
+            _arrayValues[_arrayValues.Length - 1] = value;
+
+            //just for me - control
+            Console.WriteLine($"{_arrayKeys[_arrayKeys.Length - 1]} , {_arrayValues[_arrayValues.Length - 1]} is added.(index :  {_arrayKeys.Length - 1} )");
+        }
+       
+    }
+   
+}
---
+++ Dictionaries/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> students = new MyDictionary<int, string>();
+            students.Add(122, "Ahmet Yeşim");
+            students.Add(1375, "Sevgi Sevinç");
+            students.Add(872, "Suat Varlı");
+            students.Add(1220, "Yeliz Kar");
+            students.Add(367, "Mehmet Yılmaz");
+        }
+    }
+}
